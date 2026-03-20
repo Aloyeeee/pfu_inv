7204e43e17a0c0af8bca6ddd794fbec01289ece0
Aloyeee <106309362+Aloyeeee@users.noreply.github.com>
2026-03-20 11:58:51 +0200

    Delete database directory

--- database/upd.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO locations (name)
-SELECT DISTINCT location_name 
-FROM inventory 
-WHERE location_name IS NOT NULL 
-  AND location_name != '' 
-  AND location_name NOT IN (SELECT name FROM locations);
